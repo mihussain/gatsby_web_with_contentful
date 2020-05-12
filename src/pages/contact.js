98940bd4ec7931f3cddb4ecb63951d679e2d7929
Imran <mihussain@hotmail.co.uk>
2020-05-12 06:57:43 +0100

    initial commit

---
+++ src/pages/contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <Head title="Contact" />
+            <h1>Contact</h1>
+            <p>If you have any queries regarding the portfolio, or would like to get in touch for any future projects, please do not hesitate to contact me using the form on the right. Alternatively you can also contact me using the alternative methods shown below.</p>
+
+            <div>Phone: +44(0)7540 143 107</div>
+            <div>Email: hello@imrandesigns.co.uk</div>
+        </Layout>
+    )
+}
+
+export default ContactPage
---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const PortfolioPage = () => {
+    return (
+        <Layout>
+            <Head title="Portfolio" />
+            <h1>Portfolio</h1>
+            <p>The Portfolio will be show here</p>
+        </Layout>
+    )
+}
+
+export default PortfolioPage
